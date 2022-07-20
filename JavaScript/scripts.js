e6fca895408537884b7712a6eeea5c09b452fc26
Toby-Bremner <bremner.tobias@gmail.com>
2022-07-20 08:10:56 -0300

    Added script src
    Added 2 links to to a js files for pollyfill and fetch pollyfill

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,122 +1,23 @@
-//Height is in meters. Weight is in kilograms.
+let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Bulbasaur",
-      attack: 49,
-      defence: 49,
-      height: 0.7,
-      weight: 6.9,
-      types: ["grass", "poison"],
-    },
-
-    {
-      name: "Ivysaur",
-      attack: 62,
-      defence: 63,
-      height: 1,
-      weight: 13,
-      types: ["grass", "poison"],
-    },
-
-    {
-      name: "Venusaur",
-      attack: 82,
-      defence: 83,
-      height: 2,
-      weight: 100,
-      types: ["grass", "poison"],
-    },
-
-    {
-      name: "Charmander",
-      attack: 52,
-      defence: 43,
-      height: 0.6,
-      weight: 8.5,
-      types: ["fire"],
-    },
-
-    {
-      name: "Charmeleon",
-      attack: 64,
-      defence: 58,
-      height: 1.1,
-      weight: 19,
-      types: ["fire"],
-    },
-
-    {
-      name: "Charizard",
-      attack: 84,
-      defence: 78,
-      height: 1.7,
-      weight: 90.5,
-      types: ["fire", "flying"],
-    },
-
-    {
-      name: "Squirtle",
-      attack: 48,
-      defence: 65,
-      height: 0.5,
-      weight: 9,
-      types: ["water"],
-    },
-
-    {
-      name: "Wartortle",
-      attack: 63,
-      defence: 80,
-      height: 1,
-      weight: 22.5,
-      types: ["water"],
-    },
-
-    {
-      name: "Blastoise",
-      attack: 83,
-      defence: 100,
-      height: 1.6,
-      weight: 85.5,
-      types: ["water"],
-    },
-
-    {
-      name: "Caterpie",
-      attack: 30,
-      defence: 35,
-      height: 1,
-      weight: 0.3,
-      types: ["bug"],
-    },
-
-    {
-      name: "Metapod",
-      attack: 20,
-      defence: 55,
-      height: 0.7,
-      weight: 9.9,
-      types: ["bug"],
-    },
-
-    {
-      name: "Butterfree",
-      attack: 45,
-      defence: 50,
-      height: 1.1,
-      weight: 32,
-      types: ["Bug", "flying"],
-    },
-  ];
+  let pokemonList = [];
 
   return {
     add: function (pokemon) {
-      pokemonList.push(pokemon);
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "detailsUrl" in pokemon
+      ) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log("Pokemon is not correct");
+      }
     },
     getAll: function () {
       return pokemonList;
     },
+
     addListItem: function (pokemon) {
       let pokemonList = document.querySelector(".pokemon-list");
       let listPokemon = document.createElement("li");
@@ -136,28 +37,49 @@ let pokemonRepository = (function () {
     showDetails: function (pokemon) {
       console.log(pokemon);
     },
+
+    loadList: function () {
+      return fetch(apiUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url,
+            };
+            pokemonRepository.add(pokemon);
+          });
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+    },
+    loadDetails: function (pokemon) {
+      let url = pokemon.detailsUrl;
+      return fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (details) {
+          pokemon.imageUrl = details.sprites.front_default;
+          pokemon.height = details.height;
+          pokemon.weight = details.weight;
+          pokemon.types = details.types.map((type) => type.type.name).join(",");
+          pokemon.abilities = details.abilities
+            .map((ability) => ability.ability.name)
+            .join(",");
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+    },
   };
 })();
 
-pokemonRepository.add({
-  name: "Weedle",
-  attack: 35,
-  defence: 30,
-  height: 1,
-  weight: 3.2,
-  types: ["bug", "poison"],
-});
-pokemonRepository.add({
-  name: "Pikachu",
-  attack: 55,
-  defence: 40,
-  height: 0.4,
-  weight: 13.2,
-  types: ["electric"],
-});
-
-// console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
